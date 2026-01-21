450fef6c58cca8acba273c6afc541971ecde1785
Gabriel Souza <gabrielsouza65115@gmail.com>
2026-01-21 11:40:00 -0300

    fix: Remove icon de localização

--- src/components/sections/HeroTemplateNovo.jsx
+++ src/components/sections/HeroTemplateNovo.jsx
@@ -97,9 +97,9 @@ function HeroTemplateNovo({
               <div
                 className={`inline-flex items-center gap-2 px-4 py-1.5 rounded-full border shadow-sm text-xs font-secondFont font-bold tracking-wide uppercase ${bgMinitag}`}
               >
-                <span>
+                {/* <span>
                   <MapPin className="w-4 h-4" />
-                </span>
+                </span> */}
                 {content.texts.hero.miniTag}
               </div>
 
